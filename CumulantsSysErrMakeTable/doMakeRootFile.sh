462bbdf69973545827e74cf7caeec11d170ff4b3
Yme <yghuang@mails.ccnu.edu.cn>
2023-05-13 16:47:12 +0800

    CumulantsSysErrMakeTable - v1.1.1

--- CumulantsSysErrMakeTable/doMakeRootFile.sh
+++ CumulantsSysErrMakeTable/doMakeRootFile.sh
@@ -8,11 +8,13 @@
 # :out dir: a string, path to save the out put root file
 
 FPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/19p6/18AprSys/roots
-RIDX=y5
+# RIDX=y5
 OPATH=roots
 
 if [ ! -d $OPATH ]; then
         mkdir $OPATH
 fi
 
-./MakeRootFile $FPATH $RIDX $OPATH
+for RIDX in y1 y2 y3 y4 y5 y6 y7 ; do
+        ./MakeRootFile $FPATH $RIDX $OPATH
+done;
