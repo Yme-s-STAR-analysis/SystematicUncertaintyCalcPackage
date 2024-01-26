1917a9ff93c158df9a8c043c33ea404aec8b4475
Yme <yghuang@mails.ccnu.edu.cn>
2024-01-27 00:26:14 +0100

    v2.5 cumulant batch scripts

--- CumulantsSysErrMakeTable/doGetSysErrAlter.sh
+++ CumulantsSysErrMakeTable/doGetSysErrAlter.sh
@@ -2,7 +2,7 @@
 
 # This is just a template of making LATEX table with root files.
 
-# Exec. run arguments are showing below:
+# Exec. runAlter arguments are showing below:
 # :folder path: a string, the path to the root files (root path)
 # :rapidity index: a string (indeed an integer), 1 to 7
 # :particle tag: Netp, Pro or Pbar
@@ -13,7 +13,7 @@ FPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/14p6/cumulant/240111Sys/roots
 RIDX=y0p5
 PTAG=Netp
 # VTAG=R42
-TPATH=../tables
+TPATH=../alter_tables
 
 if [ ! -d $TPATH ]; then
         mkdir $TPATH
@@ -22,21 +22,21 @@ fi
 # cumulants
 for x (1 2 3 4) {
         echo C$x
-        ./run $FPATH cum.cbwc.$RIDX $PTAG C$x $TPATH
+        ./runAlter $FPATH cum.cbwc.$RIDX $PTAG C$x $TPATH
         if [[ $PTAG != "Netp" ]] {
                 echo k$x
-                ./run $FPATH cum.cbwc.$RIDX $PTAG k$x $TPATH
+                ./runAlter $FPATH cum.cbwc.$RIDX $PTAG k$x $TPATH
         }
 }
 # cumulant ratios
 for x (21 32 42) {
         echo R$x
-        ./run $FPATH cum.cbwc.$RIDX $PTAG R$x $TPATH
+        ./runAlter $FPATH cum.cbwc.$RIDX $PTAG R$x $TPATH
 }
 # kappa ratios
 for x (21 31 41) {
         if [[ $PTAG != "Netp" ]] {
                 echo k$x
-                ./run $FPATH cum.cbwc.$RIDX $PTAG k$x $TPATH
+                ./runAlter $FPATH cum.cbwc.$RIDX $PTAG k$x $TPATH
         }
 }
