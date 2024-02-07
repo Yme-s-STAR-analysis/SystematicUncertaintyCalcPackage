83c021df9c9f1ad4283fa9228769572d05174bb9
Yme <yghuang@mails.ccnu.edu.cn>
2024-02-07 15:30:30 +0100

    v2.6 New table design With one more column (index)

--- core/FormatSystem.cxx
+++ core/FormatSystem.cxx
@@ -51,9 +51,9 @@ void FormatSystem::Print(const char* outname, const char* caption, const char* l
     // landscape env.
     // fout << "\\begin{landscape}\n";
     // header
-    fout << "\\begin{table}[]\n\\begin{tabular}{cc|ccccccccc}\n";
+    fout << "\\begin{table}[]\n\\begin{tabular}{lcc|ccccccccc}\n";
     // title row
-    fout << "\\multicolumn{2}{c|}{" << systems[0].GetTag1() << "}";
+    fout << "\\multicolumn{3}{c|}{" << systems[0].GetTag1() << "}";
     fout << "\t& 0-5\\%";
     fout << "\t& 5-10\\%";
     fout << "\t& 10-20\\%";
@@ -64,17 +64,17 @@ void FormatSystem::Print(const char* outname, const char* caption, const char* l
     fout << "\t& 60-70\\%";
     fout << "\t& 70-80\\%\t\\\\ \\hline\n";
     // default rows
-    fout << "\\multirow{2}{*}{" << systems[0].GetTag2() << "} & value ";
+    fout << "\\multirow{2}{*}{" << systems[0].GetTag2() << "} && value ";
     for (int isys=0; isys<nCent; isys++) {
         fout << "\t & " << GetFormattedFloat(systems[isys].GetDefaultValue()).Data();
     }
     fout << "\t \\\\\n";
-    fout << "\t & stat. error ";
+    fout << "\t && stat. error ";
     for (int isys=0; isys<nCent; isys++) {
         fout << "\t & " << GetFormattedFloat(systems[isys].GetDefaultError()).Data();
     }
     fout << "\t \\\\\n";
-    fout << "\t & sys. error ";
+    fout << "\t && sys. error ";
     for (int isys=0; isys<nCent; isys++) {
         fout << "\t & " << GetFormattedFloat(systems[isys].GetSigma()).Data();
     }
@@ -84,19 +84,24 @@ void FormatSystem::Print(const char* outname, const char* caption, const char* l
     for (int ivrd=0; ivrd<nvrd; ivrd++) {
         int ncuts = systems[0].GetN(ivrd);
         fout << "\\multirow{" << ncuts+2 << "}{*}";
+        fout << "{" << ivrd+1 << "} & ";
+        fout << "\\multirow{" << ncuts+2 << "}{*}";
         fout << "{" << systems[0].GetSysErrTag(ivrd) << "}";
         for (int icut=0; icut<ncuts; icut++) {
+            if (icut !=0 ) {
+                fout << "\t&";
+            }
             fout << "\t&" << systems[0].GetSysErrTag(ivrd, icut);
             for (int isys=0; isys<nCent; isys++) {
                 fout << "\t & " << GetFormattedFloat(systems[isys].GetSysErrRawValue(ivrd, icut), systems[isys].IsCutPassedBarlowCheck(ivrd, icut)).Data();
             }
             fout << "\\\\\n";
         }
-        fout << "\t& $\\Sigma$";
+        fout << "\t&\t& $\\Sigma$";
         for (int isys=0; isys<nCent; isys++) {
             fout << "\t & " << GetFormattedFloat(systems[isys].GetSysErrValue(ivrd)).Data();
         }
-        fout << "\\\\ \n\t&";
+        fout << "\\\\ \n\t&&";
         for (int isys=0; isys<nCent; isys++) {
             fout << "\t & " << Form("%.1f", systems[isys].GetSysErrValue(ivrd)*100 / systems[isys].GetDefaultValue()) << "\\\%";
         }
