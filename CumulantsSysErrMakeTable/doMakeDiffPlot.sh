a2abf4869e673df198950555b40f714da3fbf839
Yme <yghuang@mails.ccnu.edu.cn>
2024-02-08 06:59:07 +0100

    v2.8 batch scripts

--- CumulantsSysErrMakeTable/doMakeDiffPlot.sh
+++ CumulantsSysErrMakeTable/doMakeDiffPlot.sh
@@ -8,26 +8,27 @@
 # :var idx: 0 to 3 for C1 ~ C4, 4 to 6 for R21 ~ R42, 7 to 10 for k1 ~ k4, 11 to 13 fork21 ~ k41, ...
 # :cent tag: centrality tag 0 to 8
 # :fig path: to save the plot
+# :withX: with RefMult3: 0, with RefMult3X: 1
 
-FPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/14p6/cumulant/240111Sys/roots
+FPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/14p6/cumulant/240206Sys/roots
 RIDX=y0p5
 # PTAG=Netp
 # VTAG=6
 CENTIDX=0
 TPATH=../fig
+WITHX=1
 
 if [ ! -d $TPATH ]; then
         mkdir $TPATH
 fi
-
 # cumulants
 for PTAG (Netp Pro Pbar) {
     for (( VTAG=0; VTAG<7; VTAG++ )) {
-        ./MakeDiffPlot $FPATH cum.cbwc.$RIDX $PTAG $VTAG $CENTIDX $TPATH
+        ./MakeDiffPlot $FPATH $RIDX $PTAG $VTAG $CENTIDX $TPATH $WITHX
     }
     if [[ $PTAG != "Netp" ]] {
-        for (( VTAG=7; VTAG<13; VTAG++ )) {
-            ./MakeDiffPlot $FPATH cum.cbwc.$RIDX $PTAG $VTAG $CENTIDX $TPATH
+        for (( VTAG=7; VTAG<=13; VTAG++ )) {
+            ./MakeDiffPlot $FPATH $RIDX $PTAG $VTAG $CENTIDX $TPATH $WITHX
         }
     }
 }
--- CumulantsSysErrMakeTable/doMakeRootFile.sh
+++ CumulantsSysErrMakeTable/doMakeRootFile.sh
@@ -1,18 +1,19 @@
 #!/bin/zsh
 
-RPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/14p6/cumulant/240111Sys/roots
+RPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/14p6/cumulant/240206Sys/roots
 TPATH=../output
+WITHX=0
 
 if [ ! -d $TPATH ]; then
         mkdir $TPATH
 fi
 
 # y scan
-for x (1 2 3 4 5) {
-    ./MakeRootFile $RPATH cum.cbwc.y0p$x $TPATH
+for x (1 2 3 4 5 6) {
+    ./MakeRootFile $RPATH y0p$x $TPATH $WITHX
 }
 # pt scan
-./MakeRootFile $RPATH cum.cbwc.pt0p8 $TPATH
+./MakeRootFile $RPATH pt0p8 $TPATH $WITHX
 for x (0 2 4 6 8) {
-    ./MakeRootFile $RPATH cum.cbwc.pt1p$x $TPATH
+    ./MakeRootFile $RPATH pt1p$x $TPATH $WITHX
 }
