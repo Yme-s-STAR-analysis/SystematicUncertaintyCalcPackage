a6853cee3c8305fc38f15f67603848ae368a7f3c
Yme <yghuang@mails.ccnu.edu.cn>
2024-04-23 07:26:59 +0200

    v2.10: updates batch script, with X is now in a loop

--- CumulantsSysErrMakeTable/doContributionPlot.sh
+++ CumulantsSysErrMakeTable/doContributionPlot.sh
@@ -10,10 +10,9 @@
 # :save path: to save the contribution plots
 # :withX: 0 for RefMult3, 1 for RefMult3X
 
-FPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/OverAll/3cum/19/roots
+FPATH=/Path/to/root/files/folder
 RIDX=y0p5
 PTAG=Netp
-# VTAG=6
 CENTIDX=0
 TPATH=../cplot
 
@@ -21,7 +20,7 @@ if [ ! -d $TPATH ]; then
         mkdir $TPATH
 fi
 # cumulants
-for (( WITHX=0; WITHX<=1; WITHX++)) {
+for WITHX (0 1) {
     for (( VTAG=0; VTAG<7; VTAG++ )) {
         ./PlotContribution $FPATH $RIDX $PTAG $VTAG $CENTIDX $TPATH $WITHX
     }
--- CumulantsSysErrMakeTable/doMakeRootFile.sh
+++ CumulantsSysErrMakeTable/doMakeRootFile.sh
@@ -1,19 +1,23 @@
 #!/bin/zsh
 
-RPATH=/Users/huangyige/Work/DataAnalysis/star/BES2/14p6/cumulant/240206Sys/roots
+RPATH=/Path/to/root/files/folder
 TPATH=../output
 WITHX=0
 
 if [ ! -d $TPATH ]; then
-        mkdir $TPATH
+    mkdir $TPATH
 fi
 
 # y scan
 for x (1 2 3 4 5 6) {
-    ./MakeRootFile $RPATH y0p$x $TPATH $WITHX
+    for WITHX (0 1) {
+        ./MakeRootFile $RPATH y0p$x $TPATH $WITHX
+    }
 }
 # pt scan
 ./MakeRootFile $RPATH pt0p8 $TPATH $WITHX
 for x (0 2 4 6 8) {
-    ./MakeRootFile $RPATH pt1p$x $TPATH $WITHX
+    for WITHX (0 1) {
+        ./MakeRootFile $RPATH pt1p$x $TPATH $WITHX
+    }
 }
