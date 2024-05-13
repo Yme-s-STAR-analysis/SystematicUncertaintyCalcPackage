a836be085c741e0f4f7ef2661d8e4186d227ddcf
Yme <yghuang@mails.ccnu.edu.cn>
2024-05-14 00:47:54 +0200

    v2.13.1: make root file arguments are changed

--- CumulantsSysErrMakeTable/doMakeRootFile.sh
+++ CumulantsSysErrMakeTable/doMakeRootFile.sh
@@ -2,7 +2,6 @@
 
 RPATH=/Path/to/root/files/folder
 TPATH=../output
-WITHX=0
 
 if [ ! -d $TPATH ]; then
     mkdir $TPATH
@@ -10,14 +9,16 @@ fi
 
 # y scan
 for x (1 2 3 4 5 6) {
-    for WITHX (0 1) {
-        ./MakeRootFile $RPATH y0p$x $TPATH $WITHX
-    }
+    WITHX=0
+    ./MakeRootFile $RPATH y0p$x $TPATH $WITHX $nPart3
+    WITHX=1
+    ./MakeRootFile $RPATH y0p$x $TPATH $WITHX $nPart3X
 }
 # pt scan
 ./MakeRootFile $RPATH pt0p8 $TPATH $WITHX
 for x (0 2 4 6 8) {
-    for WITHX (0 1) {
-        ./MakeRootFile $RPATH pt1p$x $TPATH $WITHX
-    }
+    WITHX=0
+    ./MakeRootFile $RPATH pt1p$x $TPATH $WITHX $nPart3
+    WITHX=1
+    ./MakeRootFile $RPATH pt1p$x $TPATH $WITHX $nPart3X
 }
